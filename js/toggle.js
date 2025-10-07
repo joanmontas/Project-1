3958469e4e049937989b6cd2ccfc596bbba7cd92
pat <joanmontas1@gmail.com>
2025-10-07 19:31:47 -0400

    Added toggle Functionality

--- js/toggle.js
+++ js/toggle.js
@@ -1,13 +1,14 @@
 var on_and_off = true;
-
 function toggleButton() {
         if (on_and_off)
         {
                 console.log("Its on, turn it off");
                 on_and_off = false;
+                document.getElementsByClassName("middleDiv")[0].style.backgroundColor = "gray";
         } else {
                 on_and_off = true;
                 console.log("Its off, turn it on");
+                document.getElementsByClassName("middleDiv")[0].style.backgroundColor = "wheat";
         }
 
 }
